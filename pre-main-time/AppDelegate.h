e300cf3a546ce9f8849243cdceb62525f734c5a4
chennyhuang <793487515@qq.com>
2022-02-22 10:02:12 +0800

    commit
    commit

---
+++ pre-main-time/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  pre-main-time
+//
+//  Created by hzy on 2022/2/19.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
